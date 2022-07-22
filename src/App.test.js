f8a55298fe750c6583b359399ee9461b96c2cb97
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-22 11:07:27 -0400

    create unit tests for guess component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("App", () => {
+  afterEach(cleanup);
+
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+
+  it("renders text on the page", () => {
+    const { queryByText } = render(<App />);
+    expect(queryByText(/qween-man/i)).toBeInTheDocument();
+    expect(queryByText(/randomwordnotonsite/i)).not.toBeInTheDocument();
+  })
+
 });
---
+++ src/Guess.test.js
@@ -0,0 +1,70 @@
+import { render, cleanup } from "@testing-library/react";
+import { act } from 'react-dom/test-utils';
+import Guess from "./Guess";
+import allLetters from './constants'
+window.scrollTo = jest.fn()
+
+
+describe("Guess", () => {
+  afterEach(cleanup);
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+  afterAll(() => {
+    jest.clearAllMocks()
+  })
+
+  let state ={
+    lettersGuessed: ["b", "c"],
+    phrase: "Episode",
+    allLetters,
+    currentGuess: "a",
+    incorrect: ["b"],
+    winner: false,
+  };
+
+
+  it("renders without crashing", () => {
+    render(<Guess state={state}/>);
+  });
+
+
+  it("renders Loser component if there's a loser", () => {
+
+    const { queryByText } = render(<Guess state={state}/>);
+    expect(queryByText(/LOST/i)).not.toBeInTheDocument();
+
+    state ={
+      lettersGuessed: ["b", "c", "d", "e", "f", "g", "h", "i", "j"],
+      phrase: "Episode",
+      allLetters,
+      currentGuess: "a",
+      incorrect: ["b", "c", "d", "e", "f", "g", "h", "i", "j"],
+      winner: false,
+    };
+    render(<Guess state={state}/>)
+    expect(queryByText(/LOST/i)).toBeInTheDocument();
+  })
+
+  it("renders Winner component if there's a winner", () => {
+    const { queryByText } = render(<Guess state={state}/>);
+    expect(queryByText(/condragulations/i)).not.toBeInTheDocument();
+
+    state ={
+      lettersGuessed: ["b", "c", "d", "e", "f", "g", "h", "i", "j"],
+      phrase: "Episode",
+      allLetters,
+      currentGuess: "a",
+      incorrect: ["b", "c", "d", "e", "f", "g", "h"],
+      winner: true,
+    };
+
+    act(() => {
+      render(<Guess state={state}/>);
+    });
+    
+    expect(queryByText(/condragulations/i)).toBeInTheDocument();
+
+  })
+
+});
